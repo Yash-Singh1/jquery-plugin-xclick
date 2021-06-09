a5c7887bea046fbcdaaf8fe083d5568faaa7653f
Yash-Singh1 <saiansh2525@gmail.com>
2021-06-08 17:22:55 -0700

    Add reset test

--- test/test.js
+++ test/test.js
@@ -20,6 +20,23 @@ describe(
       });
       expect(text).toEqual('click');
     });
+
+    it('should reset every 500 ms', async () => {
+      await page.evaluate(async () => {
+        const n = Math.floor(Math.random() * 100 + 1);
+        $('button').xclick(n, () => $('#logs').html('click'));
+        for (let i = 0; i < n - 1; i++) {
+          $('button').click();
+        }
+      });
+      setTimeout(async () => {
+        const text = await page.evaluate(async () => {
+          $('button').click();
+          return $('#logs').html();
+        });
+        expect(text).toEqual('not clicked');
+      }, 500);
+    });
   },
   timeout
 );
